b6c46fa5a5e8508ad08c874d6184302a487195dc
lucasinavo-netizen <lucasinavo@gmail.com>
2026-07-01 13:08:17 +0800

    seo: rebuild casino885 for Cambodia

---
+++ api/go.js
@@ -0,0 +1,12 @@
+const DEFAULT_DESTINATION = "https://www.kh4488.com/?ch=d7057f224c";
+
+module.exports = function handler(req, res) {
+  const destination = process.env.CAMBODIA_AFFILIATE_URL || DEFAULT_DESTINATION;
+
+  res.setHeader("X-Robots-Tag", "noindex, nofollow");
+  res.setHeader("Cache-Control", "no-store");
+  res.writeHead(302, {
+    Location: destination,
+  });
+  res.end();
+};
